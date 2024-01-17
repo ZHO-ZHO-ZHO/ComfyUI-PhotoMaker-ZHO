e75064af20c399036f2ede2ade858bec1f55dfb8
ZHO-ZHO-ZHO <140084057+ZHO-ZHO-ZHO@users.noreply.github.com>
2024-01-17 20:14:36 +0800

    Update PhotoMakerNode.py V1.5 新增单张参考图输入

--- PhotoMakerNode.py
+++ PhotoMakerNode.py
@@ -20,6 +20,104 @@ def apply_style(style_name: str, positive: str, negative: str = "") -> tuple[str
         p, n = styles.get(style_name, styles[DEFAULT_STYLE_NAME])
         return p.replace("{prompt}", positive), n + ' ' + negative
 
+#single
+class PhotoMaker_Zho:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "ref_image": ("IMAGE",),
+                "base_model_path": ("STRING", {"default": "SG161222/RealVisXL_V3.0", "multiline": False}),
+                "prompt": ("STRING", {"default": "sci-fi, closeup portrait photo of a man img wearing the sunglasses in Iron man suit, face, slim body, high quality, film grain", "multiline": True}),
+                "negative_prompt": ("STRING", {"default": "asymmetry, worst quality, low quality, illustration, 3d, 2d, painting, cartoons, sketch), open mouth", "multiline": True}),
+                "style_name": (STYLE_NAMES, {"default": DEFAULT_STYLE_NAME}),
+                "style_strength_ratio": ("INT", {"default": 20, "min": 1, "max": 50, "display": "slider"}),
+                "steps": ("INT", {"default": 50, "min": 1, "max": 100, "step": 1, "display": "slider"}),
+                "guidance_scale": ("FLOAT", {"default": 5, "min": 0.1, "max": 10.0, "step": 0.1, "display": "slider"}),
+                "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("image",)
+    FUNCTION = "process_images"
+    CATEGORY = "📷PhotoMaker"
+
+    def process_images(self, base_model_path, ref_image, prompt, negative_prompt, style_name, style_strength_ratio, steps, guidance_scale, seed):
+
+        # Load base model
+        pipe = PhotoMakerStableDiffusionXLPipeline.from_pretrained(
+            base_model_path,
+            torch_dtype=torch.bfloat16,
+            use_safetensors=True,
+            variant="fp16"
+        ).to(device)
+
+        # Load PhotoMaker checkpoint
+        pipe.load_photomaker_adapter(
+            os.path.dirname(photomaker_path),
+            subfolder="",
+            weight_name=os.path.basename(photomaker_path),
+            trigger_word="img"
+        )
+        pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
+        pipe.fuse_lora()
+        
+        # Process images
+        image_np = (255. * ref_image.cpu().numpy().squeeze()).clip(0, 255).astype(np.uint8)
+        pil_image = Image.fromarray(image_np)
+        input_id_images = []
+        input_id_images.append(pil_image)
+      
+        # apply the style template
+        prompt, negative_prompt = apply_style(style_name, prompt, negative_prompt)
+      
+        start_merge_step = int(float(style_strength_ratio) / 100 * steps)
+        if start_merge_step > 30:
+            start_merge_step = 30
+
+        generator = torch.Generator(device=device).manual_seed(seed)
+
+        output = pipe(
+            prompt=prompt,
+            input_id_images=input_id_images,
+            negative_prompt=negative_prompt,
+            num_images_per_prompt=1,
+            num_inference_steps=steps,
+            start_merge_step=start_merge_step,
+            generator=generator,
+            guidance_scale=guidance_scale,
+            return_dict=False
+        )
+
+        # 检查输出类型并相应处理
+        if isinstance(output, tuple):
+            # 当返回的是元组时，第一个元素是图像列表
+            images_list = output[0]
+        else:
+            # 如果返回的是 StableDiffusionXLPipelineOutput，需要从中提取图像
+            images_list = output.images
+
+        # 转换图像为 torch.Tensor，并调整维度顺序为 NHWC
+        images_tensors = []
+        for img in images_list:
+            # 将 PIL.Image 转换为 numpy.ndarray
+            img_array = np.array(img)
+            # 转换 numpy.ndarray 为 torch.Tensor
+            img_tensor = torch.from_numpy(img_array).float() / 255.
+            # 转换图像格式为 CHW (如果需要)
+            if img_tensor.ndim == 3 and img_tensor.shape[-1] == 3:
+                img_tensor = img_tensor.permute(2, 0, 1)
+            # 添加批次维度并转换为 NHWC
+            img_tensor = img_tensor.unsqueeze(0).permute(0, 2, 3, 1)
+            images_tensors.append(img_tensor)
+
+        return images_tensors
+            
+
 #batch
 class PhotoMaker_Batch_Zho:
     def __init__(self):
@@ -124,9 +222,11 @@ class PhotoMaker_Batch_Zho:
 
 # Dictionary to export the node
 NODE_CLASS_MAPPINGS = {
+    "PhotoMaker_Zho": PhotoMaker_Zho,
     "PhotoMaker_Batch_Zho": PhotoMaker_Batch_Zho
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "PhotoMaker_Batch_Zho": "📷PhotoMaker"
+    "PhotoMaker_Zho": "📷PhotoMaker",
+    "PhotoMaker_Batch_Zho": "📷PhotoMaker Batch"
 }
